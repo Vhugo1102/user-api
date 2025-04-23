1d575aff1d6a33f4a1531f9fe182dc1ad0fd4a44
Vitor Hugo da Silva <149020824+Vhugo1102@users.noreply.github.com>
2025-04-23 13:30:05 -0300

    Projeto criado e configurado

---
+++ src/test/java/com/vhs/user_api/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhs.user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
